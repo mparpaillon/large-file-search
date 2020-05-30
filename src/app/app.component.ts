5b138dc485e66f9f124e255adcf932db0099e4a0
Michel Parpaillon <michel.parpaillon@gmail.com>
2020-05-30 12:06:51 +0200

    Move heavy lifting to webworker (~20scd to index 7 million lines)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,6 +28,7 @@ export class AppComponent {
   indexedLines: Array<[ PrimaryTitle, OriginalTitle, Year, Genre ]> = [];
   indexedLinesByYear: { [ year: number ]: number[] } = {};
   displayedColumns = [ 'primaryTitle', 'originalTitle', 'year', 'genre', 'action' ];
+  webWorkerSupported = typeof Worker !== 'undefined';
 
   constructor(
     private cd: ChangeDetectorRef,
@@ -42,85 +43,22 @@ export class AppComponent {
   }
 
   generateIndex(): void {
-    fetch('./assets/large-file.tsv').then(response => {
-      if (response.ok) {
-        this.reader = response.body.getReader();
-        this.reader.read().then(this.processText.bind(this));
-      } else {
-        this.fileState = FileState.NOT_FOUND;
-        this.cd.detectChanges();
-      }
-    });
-  }
-
-  private processText({ done, value }): Promise<void> {
-    if (done) {
-      return this.done();
-    }
-
-    const chunk = new TextDecoder('utf-8').decode(value);
-    chunk.split('\n').forEach(chunkLine => this.parseLine(chunkLine, false));
-
-    return this.reader.read().then(this.processText.bind(this));
-  }
-
-  private parseLine(chunkLine, retry = true): void {
-    const splitLine = chunkLine.split('\t');
-
-    // Ignore first line
-    if (splitLine[0] === 'tconst') {
+    if (!this.webWorkerSupported) {
       return;
     }
-
-    // If the line looks complete, we index it
-    if (splitLine.length === 9) {
-      const lineIndex = this.indexedLines.length - 1;
-      return this.indexLine(splitLine, lineIndex);
-    }
-
-    // If we get here, we have an incomplete line, but let's give it a chance (only 1 retry)
-    if (retry) {
-      // If we have some text left from the previous chunk...
-      if (this.chunkRest) {
-        // ...we fix the line by concatenating both
-        const fixedLine = this.chunkRest + chunkLine;
-        this.chunkRest = null;
-        return this.parseLine(fixedLine, false);
-      }
-
-      // ...else we save this part for the next chunk
-      this.chunkRest = chunkLine;
-    }
-  }
-
-  private indexLine(line: string[], lineIndex: number): void {
-    const primaryTitle = line[2];
-    const originalTitle = line[3];
-    const year = parseInt(line[5], 10);
-    const genre = line[8];
-
-    this.indexedLinesByYear[year] = [
-      ...(this.indexedLinesByYear[year] || []),
-      lineIndex
-    ];
-
-    // Could also be written like this (@todo Check perf diff)
-    /*
-      if (this.indexedLinesByYear[year]) {
-        this.indexedLinesByYear[year].push(lineIndex);
+    const worker = new Worker('./generate-index.worker', { type: 'module' });
+    worker.onmessage = ({ data }) => {
+      if (data === 'file-not-found') {
+        this.fileState = FileState.NOT_FOUND;
+      } else if (data === 'index-done') {
+        this.fileState = FileState.LOADED;
       } else {
-        this.indexedLinesByYear[year] = [ lineIndex ];
+        this.indexedLines.push(...data);
       }
-    */
 
-    this.indexedLines.push([ primaryTitle, originalTitle, year, genre ]);
-  }
-
-  private done(): Promise<void> {
-    this.fileState = FileState.LOADED;
-    this.cd.detectChanges();
-
-    return Promise.resolve();
+      this.cd.detectChanges();
+    };
+    // worker.postMessage('hello');
   }
 
   onSubmit(searchData: { title: string, year: number }): void {
---
+++ src/app/generate-index.worker.ts
@@ -0,0 +1,89 @@
+/// <reference lib="webworker" />
+
+// addEventListener('message', ({ data }) => {
+//   const response = `worker response to ${data}`;
+//   postMessage(response);
+// });
+
+let reader;
+let indexedLines = [];
+let chunkRest: string;
+
+console.time('indexing');
+
+fetch('./assets/large-file.tsv').then(response => {
+  if (response.ok) {
+    reader = response.body.getReader();
+    reader.read().then(processText);
+  } else {
+    postMessage('file-not-found');
+  }
+});
+
+function processText({ done, value }): Promise<void> {
+  if (done) {
+    console.timeEnd('indexing');
+    postMessage('index-done');
+    return Promise.resolve();
+  }
+
+  const chunk = new TextDecoder('utf-8').decode(value);
+  chunk.split('\n').forEach(chunkLine => parseLine(chunkLine, false));
+
+  postMessage(indexedLines);
+  indexedLines = [];
+
+  return reader.read().then(processText);
+}
+
+function parseLine(chunkLine, retry = true): void {
+  const splitLine = chunkLine.split('\t');
+
+  // Ignore first line
+  if (splitLine[0] === 'tconst') {
+    return;
+  }
+
+  // If the line looks complete, we index it
+  if (splitLine.length === 9) {
+    const lineIndex = indexedLines.length - 1;
+    return indexLine(splitLine, lineIndex);
+  }
+
+  // If we get here, we have an incomplete line, but let's give it a chance (only 1 retry)
+  if (retry) {
+    // If we have some text left from the previous chunk...
+    if (chunkRest) {
+      // ...we fix the line by concatenating both
+      const fixedLine = chunkRest + chunkLine;
+      chunkRest = null;
+      return parseLine(fixedLine, false);
+    }
+
+    // ...else we save this part for the next chunk
+    chunkRest = chunkLine;
+  }
+}
+
+function indexLine(line: string[], lineIndex: number): void {
+  const primaryTitle = line[2];
+  const originalTitle = line[3];
+  const year = parseInt(line[5], 10);
+  const genre = line[8];
+
+  // indexedLinesByYear[year] = [
+  //   ...(indexedLinesByYear[year] || []),
+  //   lineIndex
+  // ];
+
+  // Could also be written like this (@todo Check perf diff)
+  /*
+    if (indexedLinesByYear[year]) {
+      indexedLinesByYear[year].push(lineIndex);
+    } else {
+      indexedLinesByYear[year] = [ lineIndex ];
+    }
+  */
+
+  indexedLines.push([ primaryTitle, originalTitle, year, genre ]);
+}
